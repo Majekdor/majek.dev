d2e4a370876005587e18cbfac53a7960be98e43d
Kevin Barnes <kevin@donniebarnes.com>
2023-11-30 18:09:09 -0500

    Update farlands page

--- pages/farlands.js
+++ pages/farlands.js
@@ -17,7 +17,7 @@ export default function FarLands() {
                             <p>Unfortunately I do not have the v1 map at this time.</p>
                         </li>
                         <li>
-                            <a href="https://files.majek.dev/flv1homes.txt"><button><FontAwesomeIcon icon={faEye}/> View Homes</button></a> - View a list of player homes from v1.
+                            <a href="https://farlandsmc.s3.amazonaws.com/flv1homes.txt"><button><FontAwesomeIcon icon={faEye}/> View Homes</button></a> - View a list of player homes from v1.
                         </li>
                     </ul>
                 </Section>
@@ -26,13 +26,13 @@ export default function FarLands() {
                     <p>FarLands v2 ran from <b>June 28th, 2019</b> until <b>July 10th, 2020</b>.</p>
                     <ul>
                         <li>
-                            <a href="https://drive.google.com/file/d/1lfKAGB-Pqm1QSRGfxFqLgO8o5RW9PoVM/view?usp=sharing">
+                            <a href="https://farlandsmc.s3.amazonaws.com/farlandsv2.zip">
                                 <button><FontAwesomeIcon icon={faDownload} /> Download Map</button></a> - Download the v2 map.
                             <br/>
                             <p><b>Warning</b>: The v2 map is 30GB!</p>
                         </li>
                         <li>
-                            <a href="https://files.majek.dev/flv2homes.txt"><button><FontAwesomeIcon icon={faEye} /> View Homes</button></a> - View a list of player homes from v2.
+                            <a href="https://farlandsmc.s3.amazonaws.com/flv2homes.txt"><button><FontAwesomeIcon icon={faEye} /> View Homes</button></a> - View a list of player homes from v2.
                         </li>
                     </ul>
                 </Section>
@@ -41,19 +41,34 @@ export default function FarLands() {
                     <p>FarLands v3 ran from <b>July 11th, 2020</b> until <b>December 31st, 2021</b>.</p>
                     <ul>
                         <li>
-                            <a href="https://drive.google.com/file/d/1aVApMsNC1HrGqML-cNfeK839EIp_Xc2K/view?usp=sharing">
+                            <a href="https://farlandsmc.s3.amazonaws.com/farlandsv4.zip">
                                 <button><FontAwesomeIcon icon={faDownload} /> Download Map</button></a> - Download the v3 map.
                             <br/>
                             <p><b>Warning</b>: The v3 map is 40GB!</p>
                         </li>
                         <li>
-                            <a href="https://files.majek.dev/flv3homes.txt"><button><FontAwesomeIcon icon={faEye} /> View Homes</button></a> - View a list of player homes from v3.
+                            <a href="https://farlandsmc.s3.amazonaws.com/flv3homes.txt"><button><FontAwesomeIcon icon={faEye} /> View Homes</button></a> - View a list of player homes from v3.
                         </li>
                     </ul>
                 </Section>
 
                 <Section title="FarLands v4: New Heights">
-                    <p>FarLands v4 started <b>January 1st, 2022</b> and is still running. Come join us! The ip is <code>farlandsmc.net</code> and
+                    <p>FarLands v4 ran from <b>January 1st, 2022</b> until <b>June 16th, 2023</b>.</p>
+                    <ul>
+                        <li>
+                            <a href="https://farlandsmc.s3.amazonaws.com/farlandsv4.zip">
+                                <button><FontAwesomeIcon icon={faDownload} /> Download Map</button></a> - Download the v4 map.
+                            <br/>
+                            <p><b>Warning</b>: The v4 map is 45GB!</p>
+                        </li>
+                        <li>
+                            <a href="https://farlandsmc.s3.amazonaws.com/flv4homes.txt"><button><FontAwesomeIcon icon={faEye} /> View Homes</button></a> - View a list of player homes from v4.
+                        </li>
+                    </ul>
+                </Section>
+
+                <Section title="FarLands v5: Expedition">
+                    <p>FarLands v5 started <b>June 17th, 2023</b> and is still running. Come join us! The ip is <code>farlandsmc.net</code> and
                         you can join our Discord <a href="https://discord.gg/NE22xkYmQ5"><b>here</b></a>.</p>
                 </Section>
             </article>
